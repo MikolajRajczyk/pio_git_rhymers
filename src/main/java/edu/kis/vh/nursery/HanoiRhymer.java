b57f5f77375ca90bf317e71a9ffc7f80abc9bfb4
mikolajrajczyk <235958@edu.p.lodz.pl>
2022-04-14 11:07:17 +0200

    2. Refaktoryzacja kodu

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+		//Wiersze znajdujace sie powyzej (if-else) byly niepoprawnie sformatowane.
+    }
 }
+//Alt + right/left arrow <=> zmiana aktualnie otwartej karty w IDE w odpowiednio
+//prawo lub w lewo.
