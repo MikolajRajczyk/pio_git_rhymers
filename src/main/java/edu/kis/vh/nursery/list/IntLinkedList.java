7222c715f2429b751197f24a3a3f04e07e1b2ce9
mikolajrajczyk <235958@edu.p.lodz.pl>
2022-04-22 17:29:58 +0200

    10. Hermetyzacja nieprywatnych atrybutów w klasie Node

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -8,9 +8,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -25,15 +25,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return EMPTY_SIZE;
-        return last.value;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
             return EMPTY_SIZE;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
-
 }
