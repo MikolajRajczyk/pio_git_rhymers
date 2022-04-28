7ec9f195a0c872a5369aebe1fa7a3786a657eb91
mikolajrajczyk <235958@edu.p.lodz.pl>
2022-04-28 11:28:06 +0200

    13. Przeniesienie klasy Node do klasy IntLinkedList i usuniecie pliku Node.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,38 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
     private static final int EMPTY_SIZE = -1;
-    Node last;
+
+    private static class Node {
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            this.value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    private Node last;
 
     public void push(int i) {
         if (last == null)
